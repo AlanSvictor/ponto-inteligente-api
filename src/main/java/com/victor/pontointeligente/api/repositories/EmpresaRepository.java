c7b3485531c2c586a628f15e505cc7a4a7c1ad5c
ASVICTOR <alan.s.victor@gmail.com>
2019-12-04 15:54:52 -0300

    adicionado JPA e Testes

---
+++ src/main/java/com/victor/pontointeligente/api/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.victor.pontointeligente.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.victor.pontointeligente.api.entities.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true) //Por se tratar de um método apenas de consulta não trava o banco, isso melhora performance
+	Empresa findByCnpj(String cnpj);
+
+}
--- src/test/java/com/victor/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/victor/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -1,13 +1,18 @@
 package com.victor.pontointeligente.api;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test") //Ativa o profile 'test' criado em /PontoInteligente/src/main/resources/application-test.properties
 class PontoInteligenteApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
