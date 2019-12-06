3da5c36eb3bc519bdf3ad23e5d582c549d725928
ASVICTOR <alan.s.victor@gmail.com>
2019-12-06 15:31:11 -0300

    adicionado servicos

--- src/main/java/com/victor/pontointeligente/api/repositories/FuncionarioRepository.java
+++ src/main/java/com/victor/pontointeligente/api/repositories/FuncionarioRepository.java
@@ -1,5 +1,7 @@
 package com.victor.pontointeligente.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,5 +16,5 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> 
 	
 	Funcionario findByCpfOrEmail(String cpf, String email);
 	
-	Funcionario findOne(Long id);//
+	Optional<Funcionario> findById(Long id);//
 }
--- src/main/java/com/victor/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/com/victor/pontointeligente/api/repositories/LancamentoRepository.java
@@ -22,5 +22,4 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
 
 	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
-	
 }
